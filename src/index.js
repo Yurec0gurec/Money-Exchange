d6efd584985d70c8165be732950b7b951727ea71
Yuri Kovalev <yurec.0gurec99@gmail.com>
2018-09-20 16:01:34 +0300

    finally

--- src/index.js
+++ src/index.js
@@ -4,55 +4,97 @@ module.exports = function makeExchange(currency) {
     var rich = {
         error: "You are rich, my friend! We don't have so much coins for exchange"
     };
+
+    if (currency <= 0) {
+        return coins;
+    } else if (currency > 10000) {
+        return rich;
+    }
     var half = 0;
     var quart = 0;
     var dime = 0;
     var nickel = 0;
     var pennie = 0;
-    var exMoney = 0;
-   
-    while(exMoney < currency) {
-        if(currency - exMoney > 50) {
-            exMoney += 50;
-            half += 1;
-        }
-        else if((currency - exMoney < 50) && (currency - exMoney >= 25)) {
-            exMoney += 25;
-            quart += 1;
-        }
-        else if((currency - exMoney < 25) && (currency - exMoney) >= 10) {
-            exMoney += 10;
-            dime += 1;
-        }
-        else if((currency - exMoney < 10) && (currency - exMoney >= 5)) {
-            exMoney += 5;
-            nickel += 1;
-        }
-        else if((currency - exMoney < 5) && (currency - exMoney >= 0)) {
-            exMoney += 1;
-            pennie += 1;
-        }
-    }
-    if(half > 0) {
-        coins.H = half;
-    }
-     if(quart > 0) {
-        coins.Q = quart;
-    }
-    if(dime > 0) {
-        coins.D = dime;
-    }
-     if(nickel > 0) {
-        coins.N = nickel;
-    }
-     if(pennie > 0) {
-        coins.P = pennie;
-    }
-     if(currency > 10000) {
-        return rich;
-    }
-    else return coins;
+//     решение в овер9000 циклов
+//     while(currency >= 50) {
+//         currency -= 50;
+//         half++;
+//     }
+//    while(currency >= 25) {
+//        currency -= 25;
+//        quart++;
+//    }
+//    while(currency >= 10) {
+//        currency -= 10;
+//        dime++;
+//    }
+//    while(currency >= 5) {
+//        currency -= 5;
+//        nickel++;
+//    }
+//    while(currency >= 1) {
+//        currency--;
+//        pennie++;
+//    }
+//    if(half > 0) {
+//            coins.H = half;
+//        }
+//         if(quart > 0) {
+//            coins.Q = quart;
+//        }
+//        if(dime > 0) {
+//            coins.D = dime;
+//        }
+//         if(nickel > 0) {
+//            coins.N = nickel;
+//        }
+//         if(pennie > 0) {
+//            coins.P = pennie;
+//        }
+//        return coins;
     
+        while(currency > 0) {
+            if(currency >= 50) {
+                currency -= 50;
+                half += 1;
+            }
+            else if (currency >= 25) {
+                currency -= 25;
+                quart += 1;
+            }
+           else if (currency >= 10) {
+                currency -= 10;
+                dime += 1;
+            }
+            else if (currency >= 5) {
+                currency -= 5;
+                nickel += 1;
+            }
+            else if (currency >= 0) {
+                currency -= 1;
+                pennie += 1;
+            }
+        }
+        if(half > 0) {
+            coins.H = half;
+        }
+         if(quart > 0) {
+            coins.Q = quart;
+        }
+        if(dime > 0) {
+            coins.D = dime;
+        }
+         if(nickel > 0) {
+            coins.N = nickel;
+        }
+         if(pennie > 0) {
+            coins.P = pennie;
+        }
+         if(currency > 10000) {
+            return rich;
+        }
+        else return coins;
+        
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 }
